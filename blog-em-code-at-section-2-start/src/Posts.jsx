859aa85551dc12d18638926342db22dbec11214e
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2025-07-03 07:52:52 +0900

    16. Delete Post with useMutation

--- blog-em-code-at-section-2-start/src/Posts.jsx
+++ blog-em-code-at-section-2-start/src/Posts.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
@@ -10,6 +10,10 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: (postId) => deletePost(postId),
+  });
 
   useEffect(() => {
     if (currentPage < maxPostPage) {
@@ -71,7 +75,12 @@ export function Posts() {
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      {selectedPost && (
+        <PostDetail
+          post={selectedPost}
+          deleteMutation={deleteMutation.mutate}
+        />
+      )}
     </>
   );
 }
