a0a788c377b3b8c5aecb0bfbc7910e9760928870
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2025-07-12 00:22:16 +0900

    53. Code Quiz! Selector for useStaff

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import type { Staff } from "@shared/types";
 import { useQuery } from "@tanstack/react-query";
 
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
+import { filterByTreatment } from "../utils";
 
 // query function for useQuery
 async function getStaff(): Promise<Staff[]> {
@@ -18,9 +19,18 @@ export function useStaff() {
 
   const fallback: Staff[] = [];
 
+  const selectFn = useCallback(
+    (data: Staff[]) => {
+      if (filter === "all") return data;
+      return filterByTreatment(data, filter);
+    },
+    [filter]
+  );
+
   const { data: staff = fallback } = useQuery({
     queryKey: [queryKeys.staff],
     queryFn: getStaff,
+    select: selectFn,
   });
 
   return { staff, filter, setFilter };
